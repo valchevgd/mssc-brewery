016493565b39dfca731e1dfe5920edf49e672bd5
Georgi Valchev <gvalchev@neterra.net>
2022-01-22 23:12:33 +0200

    Added Create, Update & Delete Customer

--- src/main/java/com/valchevgd/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/com/valchevgd/msscbrewery/services/CustomerServiceImpl.java
@@ -14,4 +14,21 @@ public class CustomerServiceImpl implements CustomerService {
                 .name("John Doe")
                 .build();
     }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        customerDto.setId(UUID.randomUUID());
+
+        return customerDto;
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // ToDo
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        // ToDo
+    }
 }
--- src/main/java/com/valchevgd/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/valchevgd/msscbrewery/web/controller/CustomerController.java
@@ -2,12 +2,10 @@ package com.valchevgd.msscbrewery.web.controller;
 
 import com.valchevgd.msscbrewery.services.CustomerService;
 import com.valchevgd.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,4 +24,27 @@ public class CustomerController {
 
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<Object> createCustomer(@RequestBody CustomerDto customerDto) {
+        CustomerDto saved = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer/" + saved.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteCustomerById(customerId);
+    }
+
 }
