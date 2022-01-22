b7261d1a2962d71da0a8beb2b796ead1c98f18f6
Georgi Valchev <gvalchev@neterra.net>
2022-01-22 22:59:33 +0200

    Added Create, Update & Delete Beer

--- src/main/java/com/valchevgd/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/valchevgd/msscbrewery/services/BeerServiceImpl.java
@@ -1,12 +1,15 @@
 package com.valchevgd.msscbrewery.services;
 
 import com.valchevgd.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
+
     @Override
     public BeerDto getBeerById(UUID beerId) {
         return BeerDto.builder()
@@ -14,5 +17,22 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Stella")
                 .beerStyle("Pilsner")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //ToDo
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+        log.debug("Deleting beer with id " + beerId.toString());
     }
 }
--- src/main/java/com/valchevgd/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/valchevgd/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package com.valchevgd.msscbrewery.web.controller;
 
 import com.valchevgd.msscbrewery.services.BeerService;
 import com.valchevgd.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -25,5 +23,28 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId")UUID beerId) {
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createBeer(@RequestBody BeerDto beerDto) {
+        BeerDto savedBeer = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<Object> updateBeer(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteBeerById(beerId);
     }
 }
